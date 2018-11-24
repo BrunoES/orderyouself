34896d24f55854f657575d181a1cb5161d3f8fc7
Bruno Henrique <brunoh915@gmail.com>
2018-11-24 17:12:41 -0200

    Inclusao do componente QRCodeScanner na pagina de leitura do QR Code para testes (conforme instrucao 'usage' da biblioteca).

--- src/components/initQRCode.js
+++ src/components/initQRCode.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text,  Button, } from 'react-native';
+import { View, Text,  Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 
 import {  getCurrentOrder } from '../actions/AppActions';
 
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
 import _ from 'lodash';
 
 class InitQRCode extends Component {
+
+    onSuccess(e) {
+        alert("Success");
+        console.log(e);
+        /*
+        Linking
+          .openURL(e.data)
+          .catch(err => console.error('An error occured', err));
+        */
+    }
 
     constructor(props){
         super(props);
@@ -22,15 +34,48 @@ class InitQRCode extends Component {
         return (
             <View style={{ flex: 1, backgroundColor: "#ffffff" }}>
                 <Button
-                        onPress={ () => Actions.pratoPrincipal()}
-                        title="Iniciar"
-                        color="#841584"
-                        accessibilityLabel="Iniciar Processo de Pedido"
-                    />
+                    onPress={ () => Actions.pratoPrincipal()}
+                    title="Iniciar"
+                    color="#841584"
+                    accessibilityLabel="Iniciar Processo de Pedido"
+                />
+                <QRCodeScanner
+                    onRead={this.onSuccess.bind(this)}
+                    topContent={
+                    <Text style={styles.centerText}>
+                        Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+                    </Text>
+                    }
+                    bottomContent={
+                    <TouchableOpacity style={styles.buttonTouchable}>
+                        <Text style={styles.buttonText}>OK. Got it!</Text>
+                    </TouchableOpacity>
+                    }
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    centerText: {
+      flex: 1,
+      fontSize: 18,
+      padding: 32,
+      color: '#777',
+    },
+    textBold: {
+      fontWeight: '500',
+      color: '#000',
+    },
+    buttonText: {
+      fontSize: 21,
+      color: 'rgb(0,122,255)',
+    },
+    buttonTouchable: {
+      padding: 16,
+    },
+  });
 
 mapStateToProps = state => {
     const pedidoAtual = _.map(state.PedidoReducer, (val, uid) => {
